0720df327aca861cf0f00c2cda3f5394b24bed1d
Bharath_tn <steja11100@gmail.com>
2025-10-25 15:54:06 -0400

    angular_project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./components/navbar/navbar";
+import { Carousel } from "./components/carousel/carousel";
+import { Body } from './components/body/body';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Navbar, Carousel, Body],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
