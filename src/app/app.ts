a35de187c81b5673bf22352c1b49ba1073383ee6
Bharath_tn <steja11100@gmail.com>
2025-10-29 20:12:39 -0400

    changes in  navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./components/navbar/navbar";
-import { Carousel } from "./components/carousel/carousel";
 import { Body } from './components/body/body';
+import { Footer} from './components/footer/footer';
+import { Categories } from './components/categories/categories';
+import { Carousel } from './components/carousel/carousel';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, Carousel, Body],
+  imports: [Navbar,Carousel, Categories, Body, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
