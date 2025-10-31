f6c761e937f156feea8f802231ff3de5385beea6
Bharath_tn <steja11100@gmail.com>
2025-10-31 13:51:33 -0400

    created new component databinding

--- src/app/components/body/body.ts
+++ src/app/components/body/body.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { TopDeals } from '../top-deals/top-deals';
+import { Databinding } from '../databinding/databinding';
 
 @Component({
   selector: 'app-body',
-  imports: [
+  imports: [ Databinding,
     TopDeals
-  ],
+  
+],
   templateUrl: './body.html',
   styleUrl: './body.css',
 })
---
+++ src/app/components/databinding/databinding.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-databinding',
+  imports: [],
+  templateUrl: './databinding.html',
+  styleUrl: './databinding.css',
+})
+export class Databinding {
+
+}
