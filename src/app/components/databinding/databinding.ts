00d39a00cb9e79d16e6f18907ff5cc0dc94ed791
Bharath_tn <steja11100@gmail.com>
2025-10-31 23:20:59 -0400

    updated on  reference variable

--- src/app/components/databinding/databinding.ts
+++ src/app/components/databinding/databinding.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { parse } from 'path';
 
 @Component({
   selector: 'app-databinding',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './databinding.html',
   styleUrl: './databinding.css',
 })
 export class Databinding {
-  myName: string = 'virat kohli';
+  myName: string = 'Bharath';
+  imgUrl: string ='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxf0Pl2qPmxxHkrFuBPVfKMX3tHbIkKU_Y3g&s'
 
+  flag: boolean = true;
+  col_span = 2;
+
+  num1: number = 10;
+  num2: number = 20;
+
+  toggleflag(){
+    this.flag = !this.flag; // change value true to false , false to true 
+  }
+
+  addResult:any;
+  performAdd(a:any, b:any)
+  {
+    //this.addResult = a+b;
+    //this.addResult = +a + +b
+    this.addResult = Number(a) +Number(b);
+    // this.addResult = parseInt(a) + parseInt(b); //  a+b
+
+  }
 }
