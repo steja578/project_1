4db2a9b96c2ee70778b8d9a17d4e353539d75686
Bharath_tn <steja11100@gmail.com>
2025-11-04 21:53:55 -0500

    pagination updated to component

--- src/app/components/user-list/user-list.ts
+++ src/app/components/user-list/user-list.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import * as data from './users.json';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import {NgxPaginationModule} from 'ngx-pagination';
 
 @Component({
   selector: 'app-user-list',
-  imports: [],
+  imports: [FontAwesomeModule, NgxPaginationModule],
   templateUrl: './user-list.html',
   styleUrl: './user-list.css',
 })
 export class UserList {
    userArr = (data as any).default;  // inside the class
-
+  faPhone = faPhone;
+  faEnvelope = faEnvelope;
+  p: number = 1;
 }
