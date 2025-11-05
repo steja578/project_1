77de24441838bbfc1183b4cf711af15c422233c6
Bharath_tn <steja11100@gmail.com>
2025-11-05 15:10:32 -0500

    update changes

--- src/app/components/products/products.ts
+++ src/app/components/products/products.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
 import products from './products_data';
 import { FormsModule } from '@angular/forms';
+import { faStar } from '@fortawesome/free-solid-svg-icons'; 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {NgxPaginationModule} from 'ngx-pagination';
 
 @Component({
   selector: 'app-products',
   imports: [
-    FormsModule
+    FormsModule, FontAwesomeModule, NgxPaginationModule
   ],
   templateUrl: './products.html',
   styleUrl: './products.css',
 })
 export class Products {
+  faStar = faStar;
+  p: number = 1;
   filteredProducts = products;
   categoryArr: string[] = [];
   selectedCategory: string = 'all';
+  searchText:String= '' ;
 
   constructor() {
     this.categoryArr = products.map(prod => prod.category); // duplicate categories
@@ -25,5 +31,18 @@ export class Products {
     } else {
       this.filteredProducts = products.filter(product => product.category === this.selectedCategory);
     }
+  }
+
+
+
+
+  sortAsc()
+  {
+    this.filteredProducts.sort((a, b) => a.price - b.price);
+  }
+
+  sortDesc()
+  {
+     this.filteredProducts.sort((a, b) => b.price - a.price);
   }
 }
