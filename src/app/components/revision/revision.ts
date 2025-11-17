294d466695435a6270537bb1f0117e10250cc811
Bharath_tn <steja11100@gmail.com>
2025-11-16 22:24:50 -0500

    day4 rivision done

--- src/app/components/revision/revision.ts
+++ src/app/components/revision/revision.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-revision',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './revision.html',
   styleUrl: './revision.css',
 })
 export class Revision {
 
+  myName: string = 'virat';
+  imgurl: string ='https://picsum.photos/300';
+  flag:boolean=true;
+
+  toggle(){
+    this.flag=!this.flag;
+  }
 }
