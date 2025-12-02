94a028f347e4f1081f2aa1ff72a016d773477296
Bharath_tn <steja11100@gmail.com>
2025-12-02 11:39:59 -0500

    upddate

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,15 @@ import { Aboutus } from './components/aboutus/aboutus';
 import { Careers } from './components/careers/careers';
 import { Contactus } from './components/contactus/contactus';
 import { Notfound } from './components/notfound/notfound';
+import { UserDetails } from './components/user-details/user-details';
 
 export const routes: Routes = [
     {path:'home', component:Home},
     {path:'aboutus', component:Aboutus},
     {path:'careers', component:Careers},
     {path:'contactus', component:Contactus},
+    {path:'user-details/:id', component:UserDetails},//it will take paraemeter
     {path:'', component: Home},//nothing found go to home
-    {path:'**', component:Notfound },// if the url is not found
+    {path:'**', component:Notfound },// if the url is not found it will redirect to notfound component
     
 ];
--- src/app/components/aboutus/aboutus.ts
+++ src/app/components/aboutus/aboutus.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-aboutus',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './aboutus.html',
   styleUrl: './aboutus.css',
 })
 export class Aboutus {
-
+constructor(private routerObj: Router)
+{
+}
+execute(){
+console.log('processing....')
+this.routerObj.navigate(['/home'])
+  }
 }
